0edd4cabc6c15974d4e7f400be0cbffc345a656f
Micha Kops <micha.kops@gmail.com>
2014-07-18 21:36:50 +0200

    Writing xml examples added.

--- src/main/java/com/hascode/tutorial/xbeam/MavenPomParsing.java
+++ src/main/java/com/hascode/tutorial/xbeam/MavenPomParsing.java
@@ -1,6 +1,9 @@
 package com.hascode.tutorial.xbeam;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.xmlbeam.XBProjector;
 
@@ -13,5 +16,12 @@ public class MavenPomParsing {
 		pom.dependencies().forEach(dep -> {
 			System.out.println("-dependency -> " + dep.artifactId() + ":" + dep.groupId() + ":" + dep.version());
 		});
+		System.out.println("updating project version to 1.2.3..");
+		pom.project().version("1.2.3");
+
+		Path path = FileSystems.getDefault().getPath("/tmp", "changed.xml");
+		System.out.println("writing pom to file: " + path);
+		new XBProjector().io().file(path.toFile()).write(pom);
+		Files.readAllLines(path).forEach(System.out::println);
 	}
 }
--- src/main/java/com/hascode/tutorial/xbeam/projection/MavenPom.java
+++ src/main/java/com/hascode/tutorial/xbeam/projection/MavenPom.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.xmlbeam.annotation.XBDocURL;
 import org.xmlbeam.annotation.XBRead;
+import org.xmlbeam.annotation.XBWrite;
 
 @XBDocURL("resource:///pom.xml")
 public interface MavenPom {
@@ -16,6 +17,9 @@ public interface MavenPom {
 
 		@XBRead("./xbdefaultns:version")
 		String version();
+
+		@XBWrite("./version")
+		void version(String version);
 	}
 
 	@XBRead("/xbdefaultns:project")
